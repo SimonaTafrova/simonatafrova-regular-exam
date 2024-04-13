c8b05e07d0046f41bfcf8e96eef43110f8db3d10
Simona Tafrova <simonatafrova@gmail.com>
2024-04-13 14:42:31 +0300

    trying to deploy with a failing UI test once again since the Auto Deploy option was not turned off

--- tests/ui/about.test.js
+++ tests/ui/about.test.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 test('Check about page', async ({ page }) => {
   await page.goto('http://localhost:8080/about');
-  const heading = await page.$('h2');
+  const heading = await page.$('h3');
   const text = await heading.textContent();
   expect(text).toBe('About');
 });
